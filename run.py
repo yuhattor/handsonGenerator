e0afc599d2d1ea94f28a570bb14e86af94efa449
yuhattor <yuki.hattori@outlook.com>
2019-01-20 22:14:40 +0900

    revised

--- run.py
+++ run.py
@@ -1,21 +1,34 @@
 # -*- coding: utf-8 -*-
+
 # hands on generator
 from pathlib import Path
-from pprint import pprint 
 
-# 漢字とカタカナによって構成される文字列を抽出
+SCREENSHOTS_PATH= "./screenshots/*.png"
+TEMPLATE_PATH = "./template.html"
+OUTPUTP_PATH= "./output.html"
+INNER_TEMPLATE = '''
+
+  #__TitleOfSlide__ 
+
+  <img src='__ScreenshotPath__' /> 
+
+  ---
+  '''
+
 # get file list string and create array
 def pathToList(path):
   p = Path(".")
   return list(p.glob(path))
 
-# pathObjectToJson
-def jsonFileToDict(file):
-  f = open(file)
-  return f
 
-
-def dumpText(text, path):
-  with open(path, 'w') as f:
-    f.write(text)
-
+def run():
+  markDownText = ""
+  fullSlide = open(TEMPLATE_PATH).read()
+  for i, screenshot in enumerate(pathToList(SCREENSHOTS_PATH)):
+    it = INNER_TEMPLATE
+    markDownText += it.replace("__ScreenshotPath__", screenshot.as_posix()).replace("__TitleOfSlide__"," %s ." % str(i+1))
+  result = fullSlide.replace("__Contents__", markDownText)
+  
+  with open(OUTPUTP_PATH, 'w') as f:
+    f.write(result)
+run()
