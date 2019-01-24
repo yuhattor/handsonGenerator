2982d9d627616b95b4825b6385f8b155daa13407
yuhattor <yuki.hattori@outlook.com>
2019-01-24 19:08:22 +0900

    refactor

--- run.py
+++ run.py
@@ -5,34 +5,30 @@ from pathlib import Path
 SCREENSHOTS_PATH= "./screenshots/*.png"
 TEMPLATE_PATH = "./template.html"
 OUTPUTP_PATH= "./output.html"
-TITLE = "ハンズオン"
 INNER_TEMPLATE = '''
 #__TitleOfSlide__ 
 <img src='__ScreenshotPath__' /> 
 ---
 '''
 
-# get file list string and create array
-def pathToList(path):
-  p = Path(".")
-  return list(p.glob(path))
-
-def run():
-  fullContents = ""
-  templateSlide = open(TEMPLATE_PATH).read()
-  screenshots = sorted(pathToList(SCREENSHOTS_PATH))
+def generateSlides():
+  path = Path(".")
+  full_contents = ""
+  title = input("Please Enter Title: ")
+  template_slide = open(TEMPLATE_PATH).read()
+  screenshots = sorted(list(path.glob(SCREENSHOTS_PATH)))
   for i, screenshot in enumerate(screenshots):
     page = INNER_TEMPLATE
-    replaceList = {
+    replace_properties = {
       "__ScreenshotPath__": screenshot.as_posix(),
       "__TitleOfSlide__": " %s ." % str(i+1)
     }
-    for key in replaceList:
-      page = page.replace(key, replaceList[key])
-    fullContents += page
-  slide = templateSlide.replace("__Contents__", fullContents)
-  slide = slide.replace("__Title__", TITLE)
+    for key in replace_properties:
+      page = page.replace(key, replace_properties[key])
+    full_contents += page
+  slide = template_slide.replace("__Contents__", full_contents)
+  slide = slide.replace("__Title__", title)
   with open(OUTPUTP_PATH, 'w') as f:
     f.write(slide)
 
-run()
+generateSlides()
